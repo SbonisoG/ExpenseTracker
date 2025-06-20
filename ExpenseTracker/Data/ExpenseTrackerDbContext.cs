a7cd1c6455961b90c7b557b61636ae79ed9a026a
Sboniso <sboniso.sbo49@gmail.com>
2025-06-20 14:17:05 +0200

    Added two classes- Category, Transaction and A Category Controll.Installed syncFusion package

---
+++ ExpenseTracker/Data/ExpenseTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ExpenseTrackerDbContext : DbContext
+    {
+        public ExpenseTrackerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,7 +1,12 @@
+using ExpenseTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ExpenseTrackerDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
